e6df8c713c4cc5bd50ed2c4e5097675f20a34fcc
Maxwell Mansolf <maxwell.mansolf@northwestern.edu>
2022-12-06 15:26:31 -0800

    Trying again

--- R/fars_functions.R
+++ R/fars_functions.R
@@ -106,7 +106,7 @@ fars_read_years <- function(years) {
 fars_summarize_years <- function(years) {
   dat_list <- fars_read_years(years)
   dplyr::bind_rows(dat_list) %>%
-    dplyr::group_by(year, MONTH) %>%
+    dplyr::group_by(.data$year, .data$MONTH) %>%
     dplyr::summarize(n = n()) %>%
     tidyr::spread(year, n)
 }
